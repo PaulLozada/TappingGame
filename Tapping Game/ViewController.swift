f936035344c28f56ee7d362a68245affdf0c4d48
PaulLozada <paul_lozada@icloud.com>
2016-02-15 13:26:44 -0700

    Added lots of changed to application

--- Tapping Game/ViewController.swift
+++ Tapping Game/ViewController.swift
@@ -10,8 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var initialCoinValue = 0
+    var currentCoinValue = 0
+    var setCoinValue = 0
+    
+    
+    @IBOutlet weak var tapsTextField: UITextField!
+    @IBOutlet weak var startButtonOutlet: UIButton!
+    @IBOutlet weak var tapperTitleImage: UIImageView!
+    @IBOutlet weak var tapLabel: UILabel!
+    @IBOutlet weak var coinOutlet: UIButton!
+    
+    
+    @IBAction func coinButtonPressed(sender: UIButton) {
+        coinOutlet.hidden = false
+        tapLabel.hidden = false
+        currentCoinValue += 1
+        tapLabel.text = "Tapped \(currentCoinValue) times"
+        
+        if setCoinValue == currentCoinValue {
+            let alertController = UIAlertController(title: "You have won the game!", message: "CONGRATULATIONS", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+            alertController.addAction(action)
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+        }
+    }
+    
+    @IBAction func tapNumberTextField(sender: UITextField) {
+            setCoinValue = Int(sender.text!)!
+    }
+    @IBAction func startButtonPressed(sender: UIButton) {
+        tapperTitleImage.hidden = true
+        tapsTextField.hidden = true
+        startButtonOutlet.hidden = true
+        coinOutlet.hidden = false
+        tapLabel.hidden = false
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        coinOutlet.hidden = true
+        tapLabel.hidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
