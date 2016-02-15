d8f7f69ef197baf99092aefc87e724ba40a13f5b
PaulLozada <paul_lozada@icloud.com>
2016-02-15 12:19:10 -0700

    Initial Commit

---
+++ Tapping Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapping Game
+//
+//  Created by Paul Lozada on 2016-02-15.
+//  Copyright © 2016 Paul Lozada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
